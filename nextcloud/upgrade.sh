40397792f608602472df0035cad13e32d75ba619
Leo Chen <leo881003@gmail.com>
2024-01-07 22:17:31 +0800

    nextcloud: Remove variable

--- nextcloud/upgrade.sh
+++ nextcloud/upgrade.sh
@@ -9,13 +9,11 @@ run_as() {
     fi
 }
 
-NEXTCLOUD_ROOT="/var/www/nextcloud"
-
 config_version="0.0.0.0"
-if [ -f "$NEXTCLOUD_ROOT/config/config.php" ]; then
-    config_version=`php -r "require \"$NEXTCLOUD_ROOT/config/config.php\"; echo \$CONFIG[\"version\"];"`
+if [ -f "/var/www/nextcloud/config/config.php" ]; then
+    config_version=`php -r 'require "/var/www/nextcloud/config/config.php"; echo $CONFIG["version"];'`
 fi
-app_version=`php -r "require \"$NEXTCLOUD_ROOT/version.php\"; echo implode(\".\", \$OC_Version);"`
+app_version=`php -r 'require "/var/www/nextcloud/version.php"; echo implode(".", $OC_Version);'`
 
 echo "Initializing nextcloud $app_version ..."
 
@@ -23,13 +21,13 @@ echo "Initializing nextcloud $app_version ..."
 if [ "$config_version" = "0.0.0.0" ]; then
     echo "New nextcloud instance, installing default config files."
 
-    cp -n /usr/share/nextcloud/config/* "$NEXTCLOUD_ROOT/config/"
+    cp -n /usr/share/nextcloud/config/* "/var/www/nextcloud/config/"
 
     echo "running web-based installer on first connect!"
 #upgrade
 elif [ "$app_version" != "$config_version" ]; then
     echo "Upgrading nextcloud from $config_version ..."
-    run_as "php $NEXTCLOUD_ROOT/occ upgrade"
+    run_as "php /var/www/nextcloud/occ upgrade"
 fi
 
 exit 0
